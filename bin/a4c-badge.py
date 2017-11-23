ecc81f9208a5cb2556e5a2868e5087ee947d6981
Neil Jarvis <neil@jarvis.name>
2017-11-23 09:52:20 -0800

    Handle case where there are no packages branched

--- bin/a4c-badge.py
+++ bin/a4c-badge.py
@@ -8,7 +8,8 @@ try:
       execfile( '/eng/project/{}/settings'.format( os.environ[ 'WP' ] ) )
       badges.append( '{}[{}]'.format( os.environ[ 'WP' ].split( '.', 1 )[ 1 ],
                                    parent ) )
-      badges.append( ' | '.join( [ mp.split( '/', 1 )[ 0 ] for mp in memberPackages ] ) )
+      mps = globals().get('memberPackages', [])
+      badges.append( ' | '.join( [ mp.split( '/', 1 )[ 0 ] for mp in mps ] ) )
 except Exception as e:
       badges.append( ' *Error*: {}'.format( e ) )
 
