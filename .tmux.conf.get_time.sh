18251b3a20a63f38ad44de23dfb7d66d76fd2e22
Neil Jarvis <neil@jarvis.name>
2017-12-07 12:56:35 +0000

    Add TMUX config files

---
+++ .tmux.conf.get_time.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# When entered directly in .tmux.conf via "set -g status-right ...", TZ env var is ignored, so do this in a script.
+
+# Installed in .tmux.conf via:
+#  set -g status-right '#(~/bin/get_non_dublin_time.sh)'
+#  set -g status-right-length 100
+
+CA_TIME=`TZ=US/Pacific date +"CA: %H:%M"`
+NH_TIME=`TZ=US/Eastern date +"NH: %H:%M"`
+DUB_TIME=`TZ=Europer/Dublin date +"DUB: %H:%M"`
+BANGALORE_TIME=`TZ=Asia/Kolkata date +"BLR: %H:%M"`
+
+echo "$CA_TIME $NH_TIME $DUB_TIME $BANGALORE_TIME"
